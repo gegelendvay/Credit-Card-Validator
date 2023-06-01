9c923f0642bb53efe355b11cedb4119106237735
Gergely Lendvay <gege.lendvay2003@gmail.com>
2023-06-01 22:34:32 +0200

    Luhn algorithm
    Some test cases do not pass, will work on it for the next commit

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 input = input("Enter a credit card number: ")
-
-def check_card(card):
+def checkCard(card):
     card = card.strip().replace(" ", "")
 
     if not card.isdigit():
-        return 'Card number is not a digit'
+        return 'Invalid object type, input is not a number'
 
     if len(card) < 12 or len(card) > 19:
-        return 'Card number length is invalid'
+        return 'Invalid card number length'
+    
+    if str(card).startswith(('0', '1', '8600', '9704', '9860')):
+        return 'Invalid IIN range, unknown validation algorithm'
+    
+    #Luhn algorithm
+    checkSum = 0
+    for i in range(0, len(card) - 1, 2):
+        checkSum += int(card[i])
+    for i in range(1, len(card) - 1, 2):
+        multiplied = int(card[i])*2
+        for digit in str(multiplied):
+            checkSum += int(digit)
+
+    checkDigit = 10 - (checkSum % 10)
+    if checkDigit != int(card[-1]):
+        return 'Luhn algorithm validation failed'
 
     return 'Valid credit card number'
 
-print(check_card(input))
+print(checkCard(input))
